60a76a78688092969fdc4ac4d7bbc40e6b3b4479
Dakota Madden-Fong <maddenfong@gmail.com>
2021-05-05 06:48:38 -0700

    fixed import error and added admin registers

--- django_graphql_template/template_app/admin.py
+++ django_graphql_template/template_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+import django_graphql_template.template_app.models as models
 
 
+@admin.register(models.TemplateObject)
+class TemplateObjectAmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.TemplateCategory)
+class TemplateCategoryAmin(admin.ModelAdmin):
+    pass
