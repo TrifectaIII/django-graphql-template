61d5990332a12a9cc95fd59c3ad3378b168c64b9
Dakota Madden-Fong <maddenfong@gmail.com>
2021-05-08 15:00:27 -0700

    more work on admin

--- django_graphql_template/template_app/admin.py
+++ django_graphql_template/template_app/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 import django_graphql_template.template_app.models as models
 
+@admin.register(models.TemplateCategory)
+class TemplateCategoryAmin(admin.ModelAdmin):
+    search_fields = ['name']
 
 @admin.register(models.TemplateObject)
 class TemplateObjectAmin(admin.ModelAdmin):
-    pass
-
-@admin.register(models.TemplateCategory)
-class TemplateCategoryAmin(admin.ModelAdmin):
-    pass
+    list_display = ['name','price','category']
+    list_filter = ['category']
+    search_fields = ['name']
