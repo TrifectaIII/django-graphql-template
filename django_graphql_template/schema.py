a195be64fdfc7eb6fcc5381937af593e9918bbdc
Dakota Madden-Fong <maddenfong@gmail.com>
2021-05-09 16:22:25 -0700

    added comment

--- django_graphql_template/schema.py
+++ django_graphql_template/schema.py
@@ -5,6 +5,8 @@ import django_graphql_template.template_app.schema as template_app_schema
 
 class Query(
     template_app_schema.Query,
+    # Add more app-level queries here
+    
     graphene.ObjectType
 ):
     pass
