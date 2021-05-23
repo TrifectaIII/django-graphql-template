bb16b872f11d4b4c154730262f1991f98d13ab73
Dakota Madden-Fong <maddenfong@gmail.com>
2021-05-22 18:50:35 -0700

    added comments

--- django_graphql_template/template_app/schema.py
+++ django_graphql_template/template_app/schema.py
@@ -4,6 +4,8 @@ import django_graphql_template.template_app.models as models
 
 # Define GraphQL Schema for this app
 
+
+# Define types for models
 class TemplateCategoryType(DjangoObjectType):
     class Meta:
         model = models.TemplateCategory
@@ -14,15 +16,35 @@ class TemplateObjectType(DjangoObjectType):
         model = models.TemplateObject
         fields = ['id', 'name', 'price', 'category']
 
-class Query(graphene.ObjectType):
-    all_objects = graphene.List(TemplateObjectType)
-    category_by_name = graphene.Field(TemplateCategoryType, name=graphene.String(required=True))
 
+# Define query fields
+class Query(graphene.ObjectType):
+
+    # fetch all objects
+    all_objects = graphene.List(TemplateObjectType)
     def resolve_all_objects(root, info):
         return models.TemplateObject.objects.select_related('category').all()
 
+    # fetch a category given a specific id
+    object_by_id = graphene.Field(TemplateCategoryType, id=graphene.Int(required=False))
+    def resolve_object_by_id(root, info, id):
+        try:
+            return models.TemplateObject.objects.get(id=id)
+        except models.TemplateObject.DoesNotExist:
+            return None
+
+    # fetch all categories
+    all_categories = graphene.List(TemplateCategoryType)
+    def resolve_all_categories(root, info):
+        return models.TemplateObject.objects.select_related('objects').all()
+
+    # fetch a category given a specific name 
+    category_by_name = graphene.Field(TemplateCategoryType, name=graphene.String(required=False))
     def resolve_category_by_name(root, info, name):
         try:
             return models.TemplateCategory.objects.get(name=name)
         except models.TemplateCategory.DoesNotExist:
             return None
+
+
+# Define mutation fields
